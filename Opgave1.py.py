c080ad885fbb0c5cbb93484daca1f89447aec700
ZyaadW <90029140+ZyaadW@users.noreply.github.com>
2021-09-12 20:20:18 +0200

    Add files via upload

---
+++ Opgave1.py.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print(1827*312)
+print(64**2)
+naam = "Jan"
+score = "92"
+print(f"""Beste Jan, {naam}
+Je huidige score is 92 {score}.
+Met vriendelijke groeten,
+Henk""")
+naam = input("Hoe heet je?")
+print(f"Welkom", naam)
+leeftijd = input("Hoe oud ben jij?")
+print(f"Je bent 17 jaar oud", leeftijd)
+leeftijd = input("Hoe word je volgend jaar?")
+print(f"Je bent volgend jaar 18 jaar oud", leeftijd)
